578e8bc0c1cbcf20e12d047b9fd876da100d0c64
hello@isaacvargas.dev <hello@isaacvargas.dev>
2024-06-07 12:45:31 -0700

    add catppuccin macchiato theme

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
+-- [[
+-- trying to understand neovim configuratin
+-- by following typecraft's video
+-- https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&index=1&t=910s
+-- ]]
 
 
 
-
-
+-- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -17,6 +21,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-
+	-- catppuccin theme
+	-- https://github.com/catppuccin/nvim?tab=readme-ov-file
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {} }
 })
 
+
+
+-- set the color scheme to catppucine
+vim.cmd.colorscheme "catppuccin-macchiato"
