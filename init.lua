976471ef000d229b73c4db78f938bddf7d61a18b
hello@isaacvargas.dev <hello@isaacvargas.dev>
2024-06-07 17:11:19 -0700

    add telescope

--- init.lua
+++ init.lua
@@ -4,8 +4,12 @@
 -- https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&index=1&t=910s
 -- ]]
 
+-- neovim settings
+
+vim.g.mapleader = " "
 
 
+-- lazy == our plugin manager
 -- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -20,13 +24,27 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- this is where we declare our plugins for lazy
 require("lazy").setup({
 	-- catppuccin theme
 	-- https://github.com/catppuccin/nvim?tab=readme-ov-file
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {} }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {} },
+	-- telescope
+	-- https://github.com/nvim-telescope/telescope.nvim
+ 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+
 })
 
 
 
 -- set the color scheme to catppucine
 vim.cmd.colorscheme "catppuccin-macchiato"
+
+
+-- telescope , this was in the usage section of the docs
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
