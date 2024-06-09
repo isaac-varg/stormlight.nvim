07297487b72355793d3e88da1e34179bc9ef1da8
hello@isaacvargas.dev <hello@isaacvargas.dev>
2024-06-09 13:55:04 -0700

    add alpha dashboard

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,34 @@
+-- alpha.nvim, https://github.com/goolord/alpha-nvim?tab=readme-ov-file
+-- gives us a dashboard / landing / start page
+
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
