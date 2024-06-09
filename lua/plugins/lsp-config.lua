e9da583087a4ed692d969deee0c372a1f8a216e5
hello@isaacvargas.dev <hello@isaacvargas.dev>
2024-06-08 21:46:22 -0700

    configured lsp; added ui selector for telescope

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -43,6 +43,9 @@ return {
 			-- sets Shift K to provide us with hover documentation
 			-- what it actually calls was learned from ":help vim.lsp.buf"
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+  			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
 		end
 	}
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,32 @@
 -- telescope
 -- https://github.com/nvim-telescope/telescope.nvim
-return { 'nvim-telescope/telescope.nvim', 
-	tag = '0.1.6', 
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		-- telescope, this was in the usage section of the docs
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	end
+return { 
+	{ 'nvim-telescope/telescope.nvim', 
+		tag = '0.1.6', 
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			-- telescope, this was in the usage section of the docs
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end
+	},
+	-- this is an extension for telescope -- telescope ui select
+	-- https://github.com/nvim-telescope/telescope-ui-select.nvim
+	-- it gives us a nice ui for things like code actions (space ca)
+	{ 'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup {
+			  extensions = {
+			    ["ui-select"] = {
+			      require("telescope.themes").get_dropdown {
+			      }
+			    }
+			  }
+			}
+			require("telescope").load_extension("ui-select")
+		end
+	}
 }
-
