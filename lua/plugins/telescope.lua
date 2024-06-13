588bc9263e306db9781024563489104640d0f0a9
isaac-varg <hello@isaacvargas.dev>
2024-06-13 13:13:51 -0700

    add trouble

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,33 @@
 -- telescope
 -- https://github.com/nvim-telescope/telescope.nvim
-return { 
-	{ 'nvim-telescope/telescope.nvim', 
-		tag = '0.1.6', 
-		dependencies = { 'nvim-lua/plenary.nvim' },
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			-- telescope, this was in the usage section of the docs
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		end
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end,
 	},
 	-- this is an extension for telescope -- telescope ui select
 	-- https://github.com/nvim-telescope/telescope-ui-select.nvim
 	-- it gives us a nice ui for things like code actions (space ca)
-	{ 'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+	{
+		"https://github.com/nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require("telescope").setup {
-			  extensions = {
-			    ["ui-select"] = {
-			      require("telescope.themes").get_dropdown {
-			      }
-			    }
-			  }
-			}
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
 }
